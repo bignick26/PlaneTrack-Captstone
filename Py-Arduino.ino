87a1e339df65424e872a7ceb317ff56aec80f826
Nick Restivo <nickrestiv@gmail.com>
2019-04-13 16:28:37 -0400

    Functioning angle based yaw and pitch control
    Achieved through piping serial commands of a yaw angle and pitch angle into the program to position the motors

--- Py-Arduino.ino
+++ Py-Arduino.ino
@@ -1,13 +1,20 @@
+
 //Nicholas Restivo
 //https://learn.sparkfun.com/tutorials/easy-driver-hook-up-guide/all
 
 //Stepper: Black,Green,Red,Blue?? -> A+,A-,B+,B-
-#define stp 2
-#define dir 3
-#define MS1 4
-#define MS2 5
-#define EN  6
-//Pin 7 is Sleep
+#define stpX 12
+#define dirX 13
+#define MS1X 9
+#define MS2X 11
+#define ENX  10
+#define SLPX 8
+#define stpY 2
+#define dirY 5
+#define MS1Y 3
+#define MS2Y 7
+#define ENY  6
+#define SLPY 4
 
 
 //Declare variables for functions
@@ -17,22 +24,47 @@ int x = 0;
 //Step Speed
 unsigned long prevMillisX = 0;
 unsigned long prevMillisY = 0;
-int xStepDelay;
-int xStepDelay;
+unsigned long curMillisX;
+unsigned long curMillisY;
+int xStepDelay, yStepDelay;
+int xState = LOW;
+int yState = LOW;
+int y = 0;
+int inY = 0;
+
+//Position
+int currentPosX = 400; // ~800 is full circle on full step
+int currentPosY = 0; //0 - 4500 = full circle on 1/8 step
+float desPosX = 400;
+float desPosY = 0;
+
+int n;
+
+String readStr;
 
 void setup() {
-  pinMode(stp, OUTPUT);
-  pinMode(dir, OUTPUT);
-  pinMode(MS1, OUTPUT);
-  pinMode(MS2, OUTPUT);
-  pinMode(EN, OUTPUT);
+  pinMode(stpX, OUTPUT);
+  pinMode(dirX, OUTPUT);
+  pinMode(MS1X, OUTPUT);
+  pinMode(MS2X, OUTPUT);
+  pinMode(ENX, OUTPUT);
+  pinMode(SLPX, OUTPUT);
+  digitalWrite(SLPX,LOW);
+  pinMode(stpY, OUTPUT);
+  pinMode(dirY, OUTPUT);
+  pinMode(MS1Y, OUTPUT);
+  pinMode(MS2Y, OUTPUT);
+  pinMode(ENY, OUTPUT);
+  pinMode(SLPY, OUTPUT);
+  digitalWrite(SLPY,LOW);
   
   resetEDPins(); //Set step, direction, microstep and enable pins to default states
   Serial.begin(19200); //Open Serial connection for debugging
   Serial.println("Begin motor control");
   Serial.println();
   //Print function list for user selection
-  Serial.println("Use WASD for control");
+  Serial.println("Enter Yaw and Pitch coordinates bewteen 1-700 and 0-4500 respectively.");
+  Serial.println("Format: 'Yaw,Pitch' - Example: 350,2250 ");
   Serial.println();
 }
 
@@ -40,83 +72,113 @@ void setup() {
 void loop() {
 
   //Timer for delays
-  unsigned long curMillisX = millis(); //
-  unsigned long curMillisY = millis(); //
-  
+  curMillisX = millis();
+  curMillisY = millis();
+
+  //Take Input
   while(Serial.available()){
-      user_input = Serial.read(); //Read user input and trigger appropriate function
-      digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
-      if (user_input =='w')
-      {
-         StepUp();
-      }
-      else if(user_input =='s')
-      {
-        StepDown();
-      }
-      else if (user_input == '5')
-      {
-        SmallStepMode();
-      }
-      resetEDPins();
+
+    //Read user input and trigger appropriate function
+    user_input = Serial.read();
+    char c = user_input;
+    readStr += c;
+    delay(2);
+  }
+
+  //Call singular axis motion with one, but commas includes (Example: ,600 or 600,)
+  if (readStr.length() > 0) {
+    Serial.println(readStr);
+    int commaInd = readStr.indexOf(',');
+    desPosX = readStr.substring(0, commaInd).toInt(); // between 0 and 360
+    desPosY = readStr.substring(commaInd+1).toInt(); // between 0 and 180
+    pinMode(SLPX,INPUT);
+    pinMode(SLPY,INPUT);
+    desPosX = int((desPosX/360.0) * 800.0); //rounding?
+    desPosY = int((desPosY/180.0) * 4500.0);
+    Serial.println(desPosX);
+    Serial.println(desPosY);
+  }
+
+  xStepDelay = 20;
+  yStepDelay = 5; //smoothest at 20
+
+  //Step until desPosY = currentPosY at yStepDelay rate
+  digitalWrite(ENX, LOW); //Pull enable pin low to allow motor control
+  if (desPosX > currentPosX && desPosX <800 && ((unsigned long)(curMillisX - prevMillisX) >= xStepDelay))
+  {
+    digitalWrite(dirX, HIGH); //Pull direction pin High to move "+ degrees"
+    xState = !xState;
+    digitalWrite(stpX,xState);
+    currentPosX += 1;
+    prevMillisX = curMillisX;
+    Serial.println(currentPosX);
+  }
+  else if (desPosX < currentPosX && desPosX > 0 && ((unsigned long)(curMillisX - prevMillisX) >= xStepDelay)) {
+    digitalWrite(dirX, LOW);
+    xState = !xState;
+    digitalWrite(stpX,xState);
+    currentPosX -= 1;
+    prevMillisX = curMillisX;
+    Serial.println(currentPosX);
+  }
+  else if (desPosX == currentPosX) {
+    digitalWrite(stpX, LOW);
+    //digitalWrite(SLPX, LOW);
+    readStr = "";
+  }
+  else if (desPosX == 1000) { //Sleep
+    readStr = "";
+    pinMode(SLPX, OUTPUT);
+    digitalWrite(SLPX,LOW);
+  }
+
+  //Y!
+  //Step until desPosY = currentPosY at yStepDelay rate
+  digitalWrite(ENY, LOW); //Pull enable pin low to allow motor control
+  if (desPosY > currentPosY && desPosY <8000 && ((unsigned long)(curMillisY - prevMillisY) >= yStepDelay))
+  {
+    digitalWrite(dirY, HIGH); //Pull direction pin High to move "+ degrees"
+    yState = !yState;
+    digitalWrite(stpY,yState);
+    currentPosY += 1;
+    prevMillisY = curMillisY;
+    Serial.println(currentPosY);
+  }
+  else if (desPosY < currentPosY && desPosY > 0 && ((unsigned long)(curMillisY - prevMillisY) >= yStepDelay)) {
+    digitalWrite(dirY, LOW);
+    yState = !yState;
+    digitalWrite(stpY,yState);
+    currentPosY -= 1;
+    prevMillisY = curMillisY;
+    Serial.println(currentPosY);
+  }
+  else if (desPosY == currentPosY) {
+    digitalWrite(stpY, LOW);
+    //digitalWrite(SLPY, LOW);
+    readStr = "";
+  }
+  else if (desPosY == 10000) { //Sleep
+    readStr = "";
+    pinMode(SLPY, OUTPUT);
+    digitalWrite(SLPY,LOW);
+    //if greater than 90, current pos = 180, else current pos = 0
+    currentPosY = 0;
   }
 }
 
 //Reset Easy Driver pins to default states
 void resetEDPins()
 {
-  digitalWrite(stp, LOW);
-  digitalWrite(dir, LOW);
-  digitalWrite(MS1, LOW);
-  digitalWrite(MS2, LOW);
-  digitalWrite(EN, HIGH);
-}
+  digitalWrite(stpX, LOW);
+  digitalWrite(dirX, LOW);
+  digitalWrite(MS1X, LOW); //
+  digitalWrite(MS2X, LOW); //
+  digitalWrite(ENX, HIGH);
+  digitalWrite(stpY, LOW);
+  digitalWrite(dirY, LOW);
+  digitalWrite(MS1Y, HIGH); //
+  digitalWrite(MS2Y, HIGH); //
+  digitalWrite(ENY, HIGH);
 
-void StepUp()
-{
-  digitalWrite(dir, HIGH); //Pull direction pin High to move "up"
-  xStepDelay = 100;
-  
-  while(x<100) {
-    if((unsigned long)(currentMillisX - previousMillisX) >= xStepDelay)) {
-      
-    }
-  }
-
-  
-  Serial.println("Enter new option");
-  Serial.println();
-}
-
-//Forward/reverse stepping function
-void StepDown()
-{
-  digitalWrite(dir, LOW); //Pull direction pin Low to move "Down"
-  for(x= 1; x<100; x++)  //Loop the forward stepping enough times for motion to be visible
-  {
-    digitalWrite(stp,HIGH); //Trigger one step forward
-    delay(20);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-    delay(20);
-  }
-  Serial.println("Enter new option");
-  Serial.println();
-}
-
-
-// 1/8th microstep foward mode function
-void SmallStepMode()
-{
-  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
-  digitalWrite(MS1, HIGH); //Pull MS1, and MS2 high to set logic to 1/8th microstep resolution
-  digitalWrite(MS2, HIGH);
-  for(x= 1; x<8; x++)  //Loop the forward stepping enough times for motion to be visible
-  {
-    digitalWrite(stp,HIGH); //Trigger one step forward
-    delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-    delay(1);
-  }
-  Serial.println("Enter new option");
-  Serial.println();
+  // HIGH,HIGH = 1/8 step; LOW,HIGH = 1/4 step; HIGH,LOW = 1/2 step; LOW,LOW = full step (2 phase)
 }
