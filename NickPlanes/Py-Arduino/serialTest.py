4d37ea4f6f14518d84af6950205030a8b35cb495
Tyler Lee <tlee753@gmail.com>
2019-04-16 00:31:50 -0400

    updates

--- NickPlanes/Py-Arduino/serialTest.py
+++ NickPlanes/Py-Arduino/serialTest.py
@@ -1,7 +1,11 @@
 import serial
 import time
 
-port = serial.Serial(port='/dev/ttyUSB0',baudrate=19200,timeout=3.0)
-cmd = "190,40"
-time.sleep(3)
-port.write(cmd.encode('utf-8'))
+port = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, timeout=3.0)
+
+while True:
+    yaw = input("Yaw: ")
+    pitch = input("Pitch: ")
+    cmd = "{},{}".format(yaw, pitch)
+    port.write(cmd.encode('utf-8'))
+    time.sleep(3)
