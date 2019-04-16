963ede2392ef8740233c0a70d1b05228ede8cc34
Tyler Lee <tlee753@gmail.com>
2019-04-15 23:28:44 -0400

    updates

--- NickPlanes/dumpSerialRW.py
+++
@@ -1,9 +0,0 @@
-
-#import serial
-import subprocess
-
-#ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
-subprocess.check_output('dump1090')
-#subprocess.run('/home/up/Documents/NickPlanes/dump1090-serial/dump1090 --serial-port /dev/ttyS0 -format 0')
-#while true:
-#	print(repr(ser.read(10))
