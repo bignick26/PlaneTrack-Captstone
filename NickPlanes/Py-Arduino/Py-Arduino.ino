0734f10b141b29c644ff08a7d29c68d8e7ae92a2
Tyler Lee <tlee753@gmail.com>
2019-04-16 01:10:15 -0400

    updates

--- NickPlanes/Py-Arduino/Py-Arduino.ino
+++ NickPlanes/Py-Arduino/Py-Arduino.ino
@@ -7,15 +7,14 @@
 #define dirX 13
 #define MS1X 9
 #define MS2X 11
-#define ENX  10
+#define ENX 10
 #define SLPX 8
 #define stpY 2
 #define dirY 5
 #define MS1Y 3
 #define MS2Y 7
-#define ENY  6
+#define ENY 6
 #define SLPY 4
-
 
 //Declare variables for functions
 char user_input;
@@ -34,7 +33,7 @@ int inY = 0;
 
 //Position
 int currentPosX = 400; // ~800 is full circle on full step
-int currentPosY = 0; //0 - 4500 = full circle on 1/8 step
+int currentPosY = 0;   //0 - 4500 = full circle on 1/8 step
 float desPosX = 400;
 float desPosY = 0;
 
@@ -42,143 +41,153 @@ int n;
 
 String readStr;
 
-void setup() {
-  pinMode(stpX, OUTPUT);
-  pinMode(dirX, OUTPUT);
-  pinMode(MS1X, OUTPUT);
-  pinMode(MS2X, OUTPUT);
-  pinMode(ENX, OUTPUT);
-  pinMode(SLPX, OUTPUT);
-  digitalWrite(SLPX,LOW);
-  pinMode(stpY, OUTPUT);
-  pinMode(dirY, OUTPUT);
-  pinMode(MS1Y, OUTPUT);
-  pinMode(MS2Y, OUTPUT);
-  pinMode(ENY, OUTPUT);
-  pinMode(SLPY, OUTPUT);
-  digitalWrite(SLPY,LOW);
-  
-  resetEDPins(); //Set step, direction, microstep and enable pins to default states
-  Serial.begin(19200); //Open Serial connection for debugging
-  Serial.println("Begin motor control");
-  Serial.println();
-  //Print function list for user selection
-  Serial.println("Enter Yaw and Pitch coordinates bewteen 1-360 and 1-180 respectively.");
-  Serial.println("Format: 'Yaw,Pitch' - Example: 350,90 ");
-  Serial.println();
-}
-
-//Main loop
-void loop() {
-
-  //Timer for delays
-  curMillisX = millis();
-  curMillisY = millis();
-
-  //Take Input
-  while(Serial.available()){
-
-    //Read user input and trigger appropriate function
-    user_input = Serial.read();
-    char c = user_input;
-    readStr += c;
-    delay(2);
-  }
-
-  //Can even call singular axis motion with one, but commas includes (Example: ,600 or 600,)
-  if (readStr.length() > 0) {
-    Serial.println(readStr);
-    int commaInd = readStr.indexOf(',');
-    desPosX = readStr.substring(0, commaInd).toInt(); // between 0 and 360
-    desPosY = readStr.substring(commaInd+1).toInt(); // between 0 and 180
-    pinMode(SLPX,INPUT);
-    pinMode(SLPY,INPUT);
-    desPosX = int((desPosX/360.0) * 900.0); //rounding?
-    desPosY = int(((desPosY/270.0) * 4750.0)+45);
-    Serial.println(desPosX);
-    Serial.println(desPosY);
-  }
-
-  xStepDelay = 20;
-  yStepDelay = 10; //smoothest at 20
-
-  //Step until desPosY = currentPosY at yStepDelay rate
-  digitalWrite(ENX, LOW); //Pull enable pin low to allow motor control
-  if (desPosX > currentPosX && desPosX <800 && ((unsigned long)(curMillisX - prevMillisX) >= xStepDelay))
-  {
-    digitalWrite(dirX, LOW); //Pull direction pin High to move "+ degrees"
-    xState = !xState;
-    digitalWrite(stpX,xState);
-    currentPosX += 1;
-    prevMillisX = curMillisX;
-    Serial.println(currentPosX);
-  }
-  else if (desPosX < currentPosX && desPosX > 0 && ((unsigned long)(curMillisX - prevMillisX) >= xStepDelay)) {
-    digitalWrite(dirX, HIGH);
-    xState = !xState;
-    digitalWrite(stpX,xState);
-    currentPosX -= 1;
-    prevMillisX = curMillisX;
-    Serial.println(currentPosX);
-  }
-  else if (desPosX == currentPosX) {
-    digitalWrite(stpX, LOW);
-    //digitalWrite(SLPX, LOW);
-    readStr = "";
-  }
-  else if (desPosX == 1000) { //Sleep
-    readStr = "";
+void setup()
+{
+    pinMode(stpX, OUTPUT);
+    pinMode(dirX, OUTPUT);
+    pinMode(MS1X, OUTPUT);
+    pinMode(MS2X, OUTPUT);
+    pinMode(ENX, OUTPUT);
     pinMode(SLPX, OUTPUT);
-    digitalWrite(SLPX,LOW);
-  }
-
-  //Y!
-  //Step until desPosY = currentPosY at yStepDelay rate
-  digitalWrite(ENY, LOW); //Pull enable pin low to allow motor control
-  if (desPosY > currentPosY && desPosY <8000 && ((unsigned long)(curMillisY - prevMillisY) >= yStepDelay))
-  {
-    digitalWrite(dirY, HIGH); //Pull direction pin High to move "+ degrees"
-    yState = !yState;
-    digitalWrite(stpY,yState);
-    currentPosY += 1;
-    prevMillisY = curMillisY;
-    Serial.println(currentPosY);
-  }
-  else if (desPosY < currentPosY && desPosY > 0 && ((unsigned long)(curMillisY - prevMillisY) >= yStepDelay)) {
-    digitalWrite(dirY, LOW);
-    yState = !yState;
-    digitalWrite(stpY,yState);
-    currentPosY -= 1;
-    prevMillisY = curMillisY;
-    Serial.println(currentPosY);
-  }
-  else if (desPosY == currentPosY) {
-    digitalWrite(stpY, LOW);
-    //digitalWrite(SLPY, LOW);
-    readStr = "";
-  }
-  else if (desPosY == 10000) { //Sleep
-    readStr = "";
+    digitalWrite(SLPX, LOW);
+    pinMode(stpY, OUTPUT);
+    pinMode(dirY, OUTPUT);
+    pinMode(MS1Y, OUTPUT);
+    pinMode(MS2Y, OUTPUT);
+    pinMode(ENY, OUTPUT);
     pinMode(SLPY, OUTPUT);
-    digitalWrite(SLPY,LOW);
-    //if greater than 90, current pos = 180, else current pos = 0
-    currentPosY = 0;
-  }
+    digitalWrite(SLPY, LOW);
+
+    resetEDPins(); // Set step, direction, microstep and enable pins to default states
+    Serial.begin(19200); // Open Serial connection for debugging
+    Serial.println("Begin motor control");
+    Serial.println();
+    // Print function list for user selection
+    Serial.println("Enter Yaw and Pitch coordinates bewteen 1-360 and 1-180 respectively.");
+    Serial.println("Format: 'Yaw,Pitch' - Example: 350,90 ");
+    Serial.println();
 }
 
-//Reset Easy Driver pins to default states
+// Main loop
+void loop()
+{
+
+    // Timer for delays
+    curMillisX = millis();
+    curMillisY = millis();
+
+    // Take Input
+    while (Serial.available())
+    {
+
+        // Read user input and trigger appropriate function
+        user_input = Serial.read();
+        char c = user_input;
+        readStr += c;
+        delay(2);
+    }
+
+    // Can even call singular axis motion with one, but commas includes (Example: ,600 or 600,)
+    if (readStr.length() > 0)
+    {
+        Serial.println(readStr);
+        int commaInd = readStr.indexOf(',');
+        desPosX = readStr.substring(0, commaInd).toInt();  // between 0 and 360
+        desPosY = readStr.substring(commaInd + 1).toInt(); // between 0 and 180
+        pinMode(SLPX, INPUT);
+        pinMode(SLPY, INPUT);
+        desPosX = int((desPosX / 360.0) * 900.0); // rounding?
+        desPosY = int(((desPosY / 270.0) * 4750.0) + 45);
+        Serial.println(desPosX);
+        Serial.println(desPosY);
+    }
+
+    xStepDelay = 20;
+    yStepDelay = 10; // smoothest at 20
+
+    // Step until desPosY = currentPosY at yStepDelay rate
+    digitalWrite(ENX, LOW); // Pull enable pin low to allow motor control
+    if (desPosX > currentPosX && desPosX < 800 && ((unsigned long)(curMillisX - prevMillisX) >= xStepDelay))
+    {
+        digitalWrite(dirX, LOW); // Pull direction pin High to move "+ degrees"
+        xState = !xState;
+        digitalWrite(stpX, xState);
+        currentPosX += 1;
+        prevMillisX = curMillisX;
+        Serial.println(currentPosX);
+    }
+    else if (desPosX < currentPosX && desPosX > 0 && ((unsigned long)(curMillisX - prevMillisX) >= xStepDelay))
+    {
+        digitalWrite(dirX, HIGH);
+        xState = !xState;
+        digitalWrite(stpX, xState);
+        currentPosX -= 1;
+        prevMillisX = curMillisX;
+        Serial.println(currentPosX);
+    }
+    else if (desPosX == currentPosX)
+    {
+        digitalWrite(stpX, LOW);
+        // digitalWrite(SLPX, LOW);
+        readStr = "";
+    }
+    else if (desPosX == 1000)
+    { // Sleep
+        readStr = "";
+        pinMode(SLPX, OUTPUT);
+        digitalWrite(SLPX, LOW);
+    }
+
+    // Y!
+    // Step until desPosY = currentPosY at yStepDelay rate
+    digitalWrite(ENY, LOW); // Pull enable pin low to allow motor control
+    if (desPosY > currentPosY && desPosY < 8000 && ((unsigned long)(curMillisY - prevMillisY) >= yStepDelay))
+    {
+        digitalWrite(dirY, HIGH); // Pull direction pin High to move "+ degrees"
+        yState = !yState;
+        digitalWrite(stpY, yState);
+        currentPosY += 1;
+        prevMillisY = curMillisY;
+        Serial.println(currentPosY);
+    }
+    else if (desPosY < currentPosY && desPosY > 0 && ((unsigned long)(curMillisY - prevMillisY) >= yStepDelay))
+    {
+        digitalWrite(dirY, LOW);
+        yState = !yState;
+        digitalWrite(stpY, yState);
+        currentPosY -= 1;
+        prevMillisY = curMillisY;
+        Serial.println(currentPosY);
+    }
+    else if (desPosY == currentPosY)
+    {
+        digitalWrite(stpY, LOW);
+        // digitalWrite(SLPY, LOW);
+        readStr = "";
+    }
+    else if (desPosY == 10000)
+    { // Sleep
+        readStr = "";
+        pinMode(SLPY, OUTPUT);
+        digitalWrite(SLPY, LOW);
+        // if greater than 90, current pos = 180, else current pos = 0
+        currentPosY = 0;
+    }
+}
+
+// Reset Easy Driver pins to default states
 void resetEDPins()
 {
-  digitalWrite(stpX, LOW);
-  digitalWrite(dirX, LOW);
-  digitalWrite(MS1X, LOW); //
-  digitalWrite(MS2X, LOW); //
-  digitalWrite(ENX, HIGH);
-  digitalWrite(stpY, LOW);
-  digitalWrite(dirY, LOW);
-  digitalWrite(MS1Y, HIGH); //
-  digitalWrite(MS2Y, HIGH); //
-  digitalWrite(ENY, HIGH);
+    digitalWrite(stpX, LOW);
+    digitalWrite(dirX, LOW);
+    digitalWrite(MS1X, LOW);
+    digitalWrite(MS2X, LOW);
+    digitalWrite(ENX, HIGH);
+    digitalWrite(stpY, LOW);
+    digitalWrite(dirY, LOW);
+    digitalWrite(MS1Y, HIGH);
+    digitalWrite(MS2Y, HIGH);
+    digitalWrite(ENY, HIGH);
 
-  // HIGH,HIGH = 1/8 step; LOW,HIGH = 1/4 step; HIGH,LOW = 1/2 step; LOW,LOW = full step (2 phase)
+    // HIGH,HIGH = 1/8 step; LOW,HIGH = 1/4 step; HIGH,LOW = 1/2 step; LOW,LOW = full step (2 phase)
 }
--- NickPlanes/dump1090/ADSBread.py
+++ NickPlanes/dump1090/ADSBread.py
@@ -8,117 +8,116 @@ longH = -84.401
 altH = 820
 
 count = 0
-gpsH = [latH,longH,altH]
-
-#lat = 33.250
-#lon = -84.3
-#alt = 30000
-#gpsPlane = [lat, lon, alt]
+gpsH = [latH, longH, altH]
 
 fletchHex = 0
 
-arduino = serial.Serial(port='/dev/ttyUSB0',baudrate=19200,timeout=3.0)
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, timeout=3.0)
+time.sleep(1)
+
 gpsP = []
 
-numF = 0 #number of flights
-flts = [] # [[hex,timeRx,timeTx,fltnum,alt,speed,heading,lat,long],[]]
+numF = 0  # number of flights
+flts = []  # [[hex,timeRx,timeTx,fltnum,alt,speed,heading,lat,long],[]]
 hexVal = []
 hexInd = 0
-tri = 1; tti = 2; fi =3; ai = 4; si = 5; hi = 6; lati = 7; longi = 8; #indexes
-
-time.sleep(3)
+tri = 1
+tti = 2
+fi = 3
+ai = 4
+si = 5
+hi = 6
+lati = 7
+longi = 8  # indexes
 
 command = ['./dump1090', '--net', '--quiet']
-subP = subprocess.Popen(command)
+subprocess.Popen(command)
+time.sleep(1)
 
-time.sleep(3)
-reader = ['nc','127.0.0.1','30003'] # |egrep','--line','-buffered','MSG,1,|MSG,3,|MSG,4,']
-p = subprocess.Popen(reader, stdout=subprocess.PIPE)
-time.sleep(3)
+reader = ['nc', '127.0.0.1', '30003']
+subP = subprocess.Popen(reader, stdout=subprocess.PIPE)
+time.sleep(1)
 
-#loop through and filter the output of dump1090 ported to 30003 and imported as subprocess p
-while 1:
+# loop through and filter the output of dump1090 ported to 30003 and imported as subprocess p
+while True:
 
-  line = p.stdout.readline().decode('utf-8')
-  split = line.split(',') #make a list of each value (comma sperated)
-  
-  try: #search if hex value has been found before
-    hexInd = hexVal.index(split[4])
-    flts[hexInd][0] = hexVal[hexInd] #set the hex value based on hex index
-    flts[hexInd][tri] = split[7]
-    flts[hexInd][tti] = split[9]
-    
-    if split[1] == '1': #check if we get fltNum
-      flts[hexInd][fi] = split[10]
-    elif split[1] == '3': #check if we get atl,lat,long
-      flts[hexInd][ai] = split[11]
-      flts[hexInd][lati] = split[14]
-      flts[hexInd][longi] = split[15]
-    elif split[1] == '4': #check if we get speed, heading
-      flts[hexInd][si] = split[12]
-      flts[hexInd][hi] = split[13]
-    
-  except ValueError:
-    hexInd = len(hexVal)
-    hexVal.append(split[4])
-    
-    #add everything b/c of append calls suck
-    flts.append([split[4],split[7],split[9],split[10],split[11],
-    split[12],split[13],split[14],split[15]])
-    
+    line = subP.stdout.readline().decode('utf-8')
+    split = line.split(',')  # make a list of each value (comma sperated)
+
+    try:  # search if hex value has been found before
+        hexInd = hexVal.index(split[4])
+        # set the hex value based on hex index
+        flts[hexInd][0] = hexVal[hexInd]
+        flts[hexInd][tri] = split[7]
+        flts[hexInd][tti] = split[9]
+
+        if split[1] == '1':  # check if we get fltNum
+            flts[hexInd][fi] = split[10]
+        elif split[1] == '3':  # check if we get atl,lat,long
+            flts[hexInd][ai] = split[11]
+            flts[hexInd][lati] = split[14]
+            flts[hexInd][longi] = split[15]
+        elif split[1] == '4':  # check if we get speed, heading
+            flts[hexInd][si] = split[12]
+            flts[hexInd][hi] = split[13]
+
+    except ValueError:
+        hexInd = len(hexVal)
+        hexVal.append(split[4])
+
+        # add everything b/c of append calls suck
+        flts.append([split[4], split[7], split[9], split[10], split[11],
+                     split[12], split[13], split[14], split[15]])
+
 #  print(flts)ls
-  if (flts[hexInd][0] == 'A667F2'):
-    fletchHex = hexInd
-  
-#  print(flts[fletchHex])
-  #hex = A667F2
-  
-  #GPS Calc
-  try:
-    latP = float(flts[fletchHex][lati])
-    longP = float(flts[fletchHex][longi])
-    altP = float(flts[fletchHex][ai]) 
-    gpsP = [latP,longP,altP]
-    dlat = (gpsP[0] - gpsH[0])
-    dlon = (gpsP[1] - gpsH[1])
-    dist = math.sqrt(((dlat*363815.46)*(dlat*363815.46)) + ((dlon*309172.01)*(dlon*309172.01)))
-  
-    dalt = (gpsP[2] - gpsH[2])
-  
-    pitch = round(math.degrees(math.atan(dalt/dist)))
-    
-    #assuming flat earth
-    if (dlon>0): #if east 
-      yaw = 180 - math.degrees(math.atan(dlat/dlon))
-    elif ((dlon<0) & (dlat > 0)): #if west & north
-      yaw = math.degrees(math.atan(dlat/dlon)) 
-    elif ((dlon<0) & (dlat < 0)): 
-      yaw = 360 - math.degrees(math.atan(dlat/dlon))
-    elif ((dlon==0) & (dlat > 0)):
-      yaw = 90
-    elif ((dlon==0) & (dlat < 0)):
-      yaw = 270
-    yaw = round(yaw) 
-    print('latP,longH,altH: ',latH,longH,altH)
-    print('latP,longP,altP: ',latP,longP,altP)
-    print(yaw)
-    print(pitch)
-    
-    if count > 100:
-      cmd = "{},{}".format(yaw,pitch).encode('utf-8')
-    # count = 0
-      arduino.write(cmd)
-    else:
-      count += 1
-    
-  except KeyboardInterrupt:
+    if (flts[hexInd][0] == 'A667F2'):
+        fletchHex = hexInd
+
+    # GPS Calc
     try:
-      subP.terminate()
-    except OSError as e:
-      print("here")
-      print(str(e))
-    subP.wait()
-  except Exception as e:
-    print(str(e))
-    
-    
+        latP = float(flts[fletchHex][lati])
+        longP = float(flts[fletchHex][longi])
+        altP = float(flts[fletchHex][ai])
+        gpsP = [latP, longP, altP]
+        dlat = (gpsP[0] - gpsH[0])
+        dlon = (gpsP[1] - gpsH[1])
+        dist = math.sqrt(((dlat*363815.46)*(dlat*363815.46)) +
+                         ((dlon*309172.01)*(dlon*309172.01)))
+        dalt = (gpsP[2] - gpsH[2])
+        pitch = round(math.degrees(math.atan(dalt/dist)))
+
+        # assuming flat earth
+        if (dlon > 0):  # if east
+            yaw = 180 - math.degrees(math.atan(dlat/dlon))
+        elif ((dlon < 0) & (dlat > 0)):  # if west & north
+            yaw = math.degrees(math.atan(dlat/dlon))
+        elif ((dlon < 0) & (dlat < 0)):
+            yaw = 360 - math.degrees(math.atan(dlat/dlon))
+        elif ((dlon == 0) & (dlat > 0)):
+            yaw = 90
+        elif ((dlon == 0) & (dlat < 0)):
+            yaw = 270
+
+        yaw = round(yaw)
+        print('latP, longH, altH: ', latH, longH, altH)
+        print('latP, longP, altP: ', latP, longP, altP)
+        print("Yaw: ", yaw)
+        print("Pitch: ", pitch)
+
+        if count > 100: # slow down the frame rate for the arduino steppers
+            cmd = "{},{}".format(yaw, pitch).encode('utf-8')
+            count = 10
+            arduino.write(cmd)
+        else:
+            count += 1
+
+    except KeyboardInterrupt:
+        try:
+            subP.terminate()
+        except OSError as e:
+            print("keyboard exception was hit, sub process not terminated properly")
+            print(str(e))
+        subP.wait()
+    except Exception as e:
+        print("catch all exception was hit")
+        print(str(e))
